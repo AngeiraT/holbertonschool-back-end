316cbb0f5d659dd033b9e818a999474c5e35f554
AngeiraT <thaispur04@icloud.com>
2022-12-05 22:24:31 -0400

    edited order

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """ extend your Python script to export data in the CSV format """
 
-import requests
-from sys import argv
 
+if __name__ == '__main__':
+    import requests
+    from sys import argv
 
-def cvs_format():
-    """record all data to cvs"""
-    
-    
     u_id = argv[1]
     api_url = "https://jsonplaceholder.typicode.com/users/{}".format(u_id)
     api_url2 = "https://jsonplaceholder.typicode.com/todos?userId={}"\
@@ -24,6 +21,3 @@ def cvs_format():
             f.write('"{}","{}","{}","{}"\n'.format(
                 u_id, EMPLOYEE_NAME, TASK_COMPLETED_STATUS, TASK_TITLE))
 
-
-if __name__ == '__main__':
-    cvs_format()
